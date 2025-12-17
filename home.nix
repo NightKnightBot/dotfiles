2943d8b2486056d4627750749fd9b1944f9b37d2
anand <anand.panchdhari@gmail.com>
2025-12-17 20:43:21 +0530

    Fixed ^L not clearing in bash

--- home.nix
+++ home.nix
@@ -35,7 +35,8 @@ in
       "cd"
     ];
     initExtra = ''
-      bind '"\C-l": clear-screen'
+      bind -m vi-insert '"\C-l": clear-screen'
+      bind -m vi-command '"\C-l": clear-screen'
       set -o vi
       eval "$(fzf --bash)"
       eval "$(zoxide init bash)"
