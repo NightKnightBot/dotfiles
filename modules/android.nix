9c25787d48967577acd9c83a4fb662c49eb25bee
anand <anand.panchdhari@gmail.com>
2025-12-20 01:46:08 +0530

    I think i fixed the android thing

--- modules/android.nix
+++ modules/android.nix
@@ -1,19 +1,4 @@
 { config, pkgs, ... }:
-let
-  androidSdk = pkgs.androidenv.composeAndroidPackages {
-    platformVersions = [ "35" ];
-    buildToolsVersions = [ "35.0.0" ];
-    platformToolsVersion = "35.0.1";
-
-    includeEmulator = true;
-    includeSources = true;
-
-    includeNDK = true;
-    ndkVersions = [ "28.2.13676358" ];
-
-    cmdLineToolsVersion = "12.0";
-  };
-in
 {
   #### SYSTEM (NixOS) ####
 
@@ -21,17 +6,12 @@ in
 
   nixpkgs.config.android_sdk.accept_license = true;
 
-  environment.systemPackages = [
-    androidSdk.androidsdk
-  ];
-
   users.users.anand.extraGroups = [ "adbusers" ];
 
   environment.variables = {
-    ANDROID_SDK_ROOT = "${androidSdk.androidsdk}/libexec/android-sdk";
-    ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
+    ANDROID_SDK_ROOT = "/home/anand/.android-sdk/libexec/android-sdk";
+    ANDROID_HOME = "/home/anand/.android-sdk/libexec/android-sdk";
   };
-  #### USER (Home Manager) ####
 
   home-manager.users.anand =
     { pkgs, ... }:
@@ -39,7 +19,6 @@ in
       home.packages = [
         pkgs.flutter
         pkgs.android-tools
-        androidSdk.emulator
       ];
     };
 }
